28fefa8154abd4498feea8c9346938cba96060c1
Swetank U <89385145+etank0@users.noreply.github.com>
2022-11-19 15:21:12 +0530

    for lualine plugin

---
+++ .config/nvim/lua/etank/lualine.lua
@@ -0,0 +1,9 @@
+local lualine = require('lualine')
+
+lualine.setup {
+  options = {
+    theme = 'pywal-nvim',
+	component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+  },
+}
