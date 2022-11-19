f30a7b2cabc63a8cbcd3a677a3bec69ddddb918e
Swetank U <89385145+etank0@users.noreply.github.com>
2022-11-19 15:13:24 +0530

    main init.lua file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+require('etank.settings')
+require('etank.plugins')
+require('etank.highlights')
+require('etank.map')
+require('etank.bufferline')
+require('etank.lualine')
+require('etank.toggleterm')
+
+--Pywal
+--local pywal = require('pywal')
+--pywal.setup()
