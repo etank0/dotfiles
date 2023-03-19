c0e45dd5c862a0c4066c9c920095475f9b2c7a6b
Swetank U <89385145+etank0@users.noreply.github.com>
2023-03-19 14:39:21 +0530

    udpate lualine.lua

--- .config/nvim/lua/etank/lualine.lua
+++ .config/nvim/lua/etank/lualine.lua
@@ -2,7 +2,7 @@ local lualine = require('lualine')
 
 lualine.setup {
   options = {
-    theme = 'pywal-nvim',
+    theme = 'catppuccin',
 	component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
   },
