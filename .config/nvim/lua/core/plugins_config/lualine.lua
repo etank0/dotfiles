b66cd53b801f270ffab60b256bd2ddf845f70b38
Swetank Upadhyay <swetank.690@gmail.com>
2024-08-09 19:47:57 +0530

    remove: old plugin files

--- .config/nvim/lua/core/plugins_config/lualine.lua
+++
@@ -1,9 +0,0 @@
-local lualine = require('lualine')
-
-lualine.setup {
-    options = {
-        theme = require('material.lualine'),
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-    },
-}
