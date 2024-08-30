5ec543dfc8dac22b67897a494f0946424b5fd95b
Swetank Upadhyay <swetank.690@gmail.com>
2024-08-30 09:09:37 +0530

    change: nvim none-ls

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -11,8 +11,24 @@ return {
                 -- formatting
                 -- null_ls.builtins.formatting.stylua,
                 -- null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.prettier,
-                require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+                -- null_ls.builtins.formatting.prettier,
+                -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+                -- Formatting for JavaScript, TypeScript, CSS, etc.
+                --
+                -- C++ Formatting
+                null_ls.builtins.formatting.clang_format.with({
+                    extra_args = { "--style", "{IndentWidth: 4}" },
+                    filetypes = { "cpp", "c" },
+                }),
+
+                -- Web Development
+                null_ls.builtins.formatting.prettier.with({
+                    filetypes = { "javascript", "typescript", "css", "scss", "html", "json", "yaml", "markdown" },
+                }),
+                -- -- JavaScript and TypeScript Linting
+                -- null_ls.builtins.diagnostics.eslint.with({
+                --     filetypes = { "javascript", "typescript" },
+                -- }),
             },
         })
     end,
