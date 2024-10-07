876e1b83a99009f6cd398bdd77cc718827bb4b74
Swetank Upadhyay <swetank.690@gmail.com>
2024-10-07 16:30:35 +0530

    change: nvim diffview keymaps

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,7 +1,7 @@
 return {
     "NeogitOrg/neogit",
     dependencies = {
-        "nvim-lua/plenary.nvim", -- required
+        "nvim-lua/plenary.nvim",  -- required
         "sindrets/diffview.nvim", -- optional - Diff integration
 
         -- Only one of these is needed, not both.
@@ -10,5 +10,7 @@ return {
     config = function()
         require("neogit").setup()
         vim.keymap.set("n", "<leader>gs", require("neogit").open, { silent = true, noremap = true })
+        vim.keymap.set("n", "<leader>dv", function() vim.cmd.DiffviewOpen() end, { silent = true, noremap = true })
+        vim.keymap.set("n", "<leader>dc", function() vim.cmd.DiffviewClose() end, { silent = true, noremap = true })
     end,
 }
