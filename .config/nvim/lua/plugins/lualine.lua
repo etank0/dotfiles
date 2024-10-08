cefbc97de131373f12413c4bf54e1a710438aa79
Swetank Upadhyay <swetank.690@gmail.com>
2024-10-08 15:00:57 +0530

    change: neovim lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,14 +1,59 @@
 return {
-	"nvim-lualine/lualine.nvim",
+    "nvim-lualine/lualine.nvim",
     priority = 1000,
     config = function()
         require("lualine").setup({
             options = {
                 theme = require('material.lualine'),
-                component_separators = { left = '', right = '' },
-                -- section_separators = { left = '', right = '' },
+                component_separators = { left = '', right = '' },
                 section_separators = { left = '', right = '' },
+                -- section_separators = { left = '', right = '' },
+                -- component_separators = { left = '', right = '' },
             },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = {
+                    -- Show the relative file path
+                    {
+                        'filename',
+                        path = 1
+                    },
+                },
+                lualine_x = {
+                    {
+                        function()
+                            local clients = vim.lsp.get_active_clients()
+                            if next(clients) == nil then
+                                return 'No LSP'
+                            end
+                            local names = {}
+                            for _, client in pairs(clients) do
+                                table.insert(names, client.name)
+                            end
+                            return table.concat(names, ', ')
+                        end,
+                        icon = ' LSP:',
+                    },
+                    'encoding', 'fileformat', 'filetype',
+                    {
+                        -- Show current directory (workspace) in lualine_x
+                        function()
+                            local cwd = vim.fn.getcwd()
+                            return vim.fn.fnamemodify(cwd, ':t')
+                        end,
+                        icon = ' ',
+                    },
+
+                },
+                lualine_y = {
+                    'progress',
+                },
+                lualine_z = {
+                    'location',
+                },
+            }
         })
     end
 }
+
