4e751ba1f10f986a803cd1c97aad98e706e8d926
Swetank Upadhyay <swetank.690@gmail.com>
2025-02-03 14:56:48 +0530

    change: nvim vscode settings

--- .config/nvim/lua/core/vscode.lua
+++ .config/nvim/lua/core/vscode.lua
@@ -5,46 +5,44 @@ local vscode = require("vscode")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
------------------ Builtin VsCode ------------------
--- -- Files
--- vim.keymap.set({'n', 'v'}, "<leader>ff", ":call VSCodeNotify('workbench.action.quickOpen')<CR>");
---
--- -- Live Grep
--- vim.keymap.set({'n', 'v'}, "<leader>fg", ":call VSCodeNotify('workbench.action.findInFiles')<CR>");
-
---- Find it Faster ---
--- Find Files
+----------------- Old Syntax Example -----------------
 -- vim.keymap.set({ "n", "v" }, "<leader>ff", ":call VSCodeNotify('find-it-faster.findFiles')<CR>")
-vim.keymap.set({ "n", "v" }, "<leader>ff", function() vscode.action("find-it-faster.findFiles") end)
 
--- -- Live Grep
--- vim.keymap.set("n", "<leader>fg", ":call VSCodeNotify('find-it-faster.findWithinFiles')<CR>")
-vim.keymap.set({ "n", "v" }, "<leader>fg", function() vscode.action("find-it-faster.findWithinFiles") end)
+----------------- Builtin VsCode ------------------
+-- Find Files
+-- vim.keymap.set({'n', 'v'}, "<leader>ff",
+--     function() vscode.action('workbench.action.quickOpen') end);
+vim.keymap.set({ "n", "v" }, "<leader>ff",
+    function() vscode.action("find-it-faster.findFiles") end)
+
+-- Live Grep
+-- vim.keymap.set({'n', 'v'}, "<leader>fg",
+--     function() vscode.action('workbench.action.findInFiles') end);
+vim.keymap.set({ "n", "v" }, "<leader>fg",
+    function() vscode.action("find-it-faster.findWithinFiles") end)
 
 -- Find Open Buffers
-vim.keymap.set({ "n", "v" }, "<leader>fb", function() vscode.action("workbench.action.showAllEditorsByMostRecentlyUsed") end)
+vim.keymap.set({ "n", "v" }, "<leader>fb",
+    function() vscode.action("workbench.action.showAllEditorsByMostRecentlyUsed") end)
 
 -- Find Recently Opened buffers in the project/directory
-vim.keymap.set({ "n", "v" }, "<leader>fo", function() vscode.action("workbench.action.quickOpen") end)
+vim.keymap.set({ "n", "v" }, "<leader>fo",
+    function() vscode.action("workbench.action.quickOpen") end)
 
 -- Match Tags
--- vim.keymap.set("n", "<leader>m", ":call VSCodeNotify('editor.emmet.action.matchTag')<CR>")
-vim.keymap.set("n", "<leader>m", function() vscode.action("editor.emmet.action.matchTag") end)
+vim.keymap.set("n", "<leader>m",
+    function() vscode.action("editor.emmet.action.matchTag") end)
 
 -- Git stuff
--- vim.keymap.set("n", "<leader>gs", ":call VSCodeNotify('workbench.view.scm')<CR>")
-vim.keymap.set("n", "<leader>gs", function() vscode.action("workbench.view.scm") end)
+vim.keymap.set("n", "<leader>gs",
+    function() vscode.action("workbench.view.scm") end)
 
--- vim.keymap.set("n", "<leader>tb", ":call VSCodeNotify('gitlens.toggleLineBlame')<CR>")
-vim.keymap.set("n", "<leader>tb", function() vscode.action("gitlens.toggleLineBlame") end)
+vim.keymap.set("n", "<leader>tb",
+    function() vscode.action("gitlens.toggleLineBlame") end)
 
--- vim.keymap.set("n", "<leader>tB", ":call VSCodeNotify('gitlens.toggleFileBlame')<CR>")
 vim.keymap.set("n", "<leader>tB", function() vscode.action("gitlens.toggleFileBlame") end)
 
 -- Formatting
--- vim.keymap.set("n", "<leader>lf", "<C-S-i>")
--- vim.keymap.set("n", "<leader>lf", ":call VSCodeNotify('editor.action.formatDocument')<CR>")
--- vim.keymap.set("v", "<leader>lf", ":call VSCodeNotify('editor.action.formatSelection')<CR>")
 vim.keymap.set("n", "<leader>lf", function() vscode.action("editor.action.formatDocument") end)
 vim.keymap.set("v", "<leader>lf", function() vscode.action("editor.action.formatSelection") end)
 
@@ -54,12 +52,18 @@ vim.keymap.set({ "n", "v" }, "<leader><tab>", function()
 end)
 
 -- Resize commands
-vim.keymap.set("n", "<C-Up>", function() vscode.action("workbench.action.decreaseViewHeight") end)
-vim.keymap.set("n", "<C-Down>", function() vscode.action("workbench.action.increaseViewHeight") end)
-vim.keymap.set("n", "<C-Left>", function() vscode.action("workbench.action.decreaseViewHeight") end)
-vim.keymap.set("n", "<C-Right>", function() vscode.action("workbench.action.increaseViewHeight") end)
-vim.keymap.set("n", "<leader>=", function() vscode.action("workbench.action.evenEditorWidths") end)
-vim.keymap.set("n", "<leader>-", function() vscode.action("workbench.action.toggleEditorWidths") end)
+vim.keymap.set("n", "<C-Up>",
+    function() vscode.action("workbench.action.decreaseViewHeight") end)
+vim.keymap.set("n", "<C-Down>",
+    function() vscode.action("workbench.action.increaseViewHeight") end)
+vim.keymap.set("n", "<C-Left>",
+    function() vscode.action("workbench.action.decreaseViewHeight") end)
+vim.keymap.set("n", "<C-Right>",
+    function() vscode.action("workbench.action.increaseViewHeight") end)
+vim.keymap.set("n", "<leader>=",
+    function() vscode.action("workbench.action.evenEditorWidths") end)
+vim.keymap.set("n", "<leader>-",
+    function() vscode.action("workbench.action.toggleEditorWidths") end)
 
 -- Navigation commands
 vim.keymap.set("n", "<leader>h", function() vscode.action("workbench.action.navigateLeft") end)
@@ -74,11 +78,14 @@ vim.keymap.set("n", "<leader>o", function() vscode.action("workbench.action.join
 
 -- Window focus command
 vim.keymap.set("n", "<leader>w", function() vscode.action("workbench.action.focusNextGroup") end)
-vim.keymap.set("n", "<leader>p", function() vscode.action("workbench.action.focusPreviousGroup") end)
+vim.keymap.set("n", "<leader>p",
+    function() vscode.action("workbench.action.focusPreviousGroup") end)
 
 -- Close Window/Tab
-vim.keymap.set("n", "<leader>dd", function() vscode.action('workbench.action.closeActiveEditor') end)
-vim.keymap.set("n", "<leader>q", function() vscode.action('workbench.action.closeActiveEditor') end)
+vim.keymap.set("n", "<leader>dd",
+    function() vscode.action('workbench.action.closeActiveEditor') end)
+vim.keymap.set("n", "<leader>q",
+    function() vscode.action('workbench.action.closeActiveEditor') end)
 
 -- Errorlens/Inlay Hints
 vim.keymap.set("n", "<leader>tv", function() vscode.action('errorLens.toggle') end)
@@ -87,10 +94,10 @@ vim.keymap.set("n", "<leader>tv", function() vscode.action('errorLens.toggle') e
 vim.keymap.set("n", "<leader>cd", function() vscode.action('editor.action.showHover') end)
 ----------------- Native Neovim ------------------
 -- Buffer Manipulations
-vim.keymap.set("n", "<S-l>", ":Tabnext<CR>")
-vim.keymap.set("n", "<S-h>", ":Tabprevious<CR>")
 -- vim.keymap.set("n", "<S-h>", ":bp<CR>")
 -- vim.keymap.set("n", "<S-l>", ":bn<CR>")
+vim.keymap.set("n", "<S-l>", ":Tabnext<CR>")
+vim.keymap.set("n", "<S-h>", ":Tabprevious<CR>")
 
 -- Diagnostic
 vim.keymap.set("n", "<leader>dh", vim.diagnostic.open_float)
@@ -104,3 +111,4 @@ vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
 -- vim.keymap.set("n", "<C-d>", "<C-d>zz")
 -- vim.keymap.set("n", "<C-f>", "<C-f>zz")
 -- vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
