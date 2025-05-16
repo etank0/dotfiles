ab591ccf645232da119afad5320dda141a545e46
Swetank Upadhyay <swetank.690@gmail.com>
2025-05-16 16:53:21 +0530

    add: ripgrep & fzf colors

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,3 +1,7 @@
 # Set FZF_DEFAULT_OPTS with customized key bindings
-export FZF_DEFAULT_OPTS="--bind 'ctrl-y:preview-up,ctrl-e:preview-down,ctrl-b:preview-page-up,ctrl-f:preview-page-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,shift-up:preview-top,shift-down:preview-bottom,alt-up:half-page-up,alt-down:half-page-down'"
+alias fzfb="fzf --preview 'bat --style=numbers --color=always {}'"
+export FZF_DEFAULT_OPTS="\
+--color=hl:#82aaff,fg+:#eeffff,bg+:#414141,hl+:#82aaff,info:#ffcb6b,prompt:#c3e88d,pointer:#ffcc00,marker:#ffcc00,spinner:#89ddff,header:#616161 \
+--cycle --pointer=▎ --marker=▎ \
+--bind=ctrl-y:preview-up,ctrl-e:preview-down,ctrl-b:preview-page-up,ctrl-f:preview-page-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,shift-up:preview-top,shift-down:preview-bottom,alt-up:half-page-up,alt-down:half-page-down"
 
