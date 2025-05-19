0d43d8932f1483a7ab57d88e8dfde217063e3532
Swetank Upadhyay <swetank.690@gmail.com>
2025-05-19 18:54:59 +0530

    fix: kitty memory icon

--- .config/mpv/scripts/autosub.lua
+++ .config/mpv/scripts/autosub.lua
@@ -3,7 +3,7 @@
 --=============================================================================
 --          This script uses Subliminal to download subtitles,
 --          so make sure to specify your system's Subliminal location below:
-local subliminal = '/home/swetank/.local/bin/subliminal'
+local subliminal = '$HOME/.local/bin/subliminal'
 --=============================================================================
 -->>    SUBTITLE LANGUAGE:
 --=============================================================================
