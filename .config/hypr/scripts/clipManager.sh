f533954a747c451f9b1aae706dd9113ecb8787e0
Swetank Upadhyay <swetank.690@gmail.com>
2025-05-22 06:06:21 +0530

    rofi: emoji picker

--- .config/hypr/scripts/clipManager.sh
+++ .config/hypr/scripts/clipManager.sh
@@ -9,35 +9,35 @@
 while true; do
     result=$(
         rofi -dmenu \
+            -p "   Clipboard" \
             -kb-custom-1 "Control-Delete" \
             -kb-custom-2 "Alt-Delete" \
             -theme ~/.config/rofi/themes/clip-raycast.rasi < <(cliphist list)
     )
 
     case "$?" in
-        1)
+    1)
+        exit
+        ;;
+    0)
+        case "$result" in
+        "")
+            continue
+            ;;
+        *)
+            cliphist decode <<<"$result" | wl-copy
+            notify-send "Clipboard" "Copied to clipboard!"
             exit
             ;;
-        0)
-            case "$result" in
-                "")
-                    continue
-                    ;;
-                *)
-                    cliphist decode <<<"$result" | wl-copy
-                    notify-send "Clipboard" "Copied to clipboard!"
-                    exit
-                    ;;
-            esac
-            ;;
-        10)
-            cliphist delete <<<"$result"
-            notify-send "Clipboard" "Entry deleted!"
-            ;;
-        11)
-            cliphist wipe
-            notify-send "Clipboard" "Clipboard History cleared!"
-            ;;
+        esac
+        ;;
+    10)
+        cliphist delete <<<"$result"
+        notify-send "Clipboard" "Entry deleted!"
+        ;;
+    11)
+        cliphist wipe
+        notify-send "Clipboard" "Clipboard History cleared!"
+        ;;
     esac
 done
-
---
+++ .config/hypr/scripts/emoji_picker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+EMOJI_TSV="$HOME/.config/rofi/assets/emojis.tsv"
+
+selected=$(awk -F'\t' '{print $1 " : " $2}' "$EMOJI_TSV" | rofi -dmenu \
+    -theme ~/.config/rofi/themes/clip-raycast.rasi \
+    -i \
+    -p "󰞅   Emoji Picker" \
+    -columns 1 \
+    -matching "normal")
+
+[ -z "$selected" ] && exit
+
+emoji=$(echo "$selected" | awk -F' : ' '{print $1}')
+
+# Copy to clipboard
+echo -n "$emoji" | wl-copy
+
+# Type the emoji
+wtype $emoji
