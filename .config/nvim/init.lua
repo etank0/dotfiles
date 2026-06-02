4bfcf9a87164bb7203911b86e841436f39c5f688
Swetank Upadhyay <swetank.690@gmail.com>
2026-06-03 03:49:36 +0530

    change: nvim settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,6 @@
 if vim.g.vscode == nil then
     require("core.settings")
     require("core.keymaps")
-    require("core.highlights")
     require("core.lazyconfig")
     require("core.lsp")
 else
--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -8,11 +8,6 @@ return {
         check_ts = true,
         disable_filetype = { "TelescopePrompt" },
       })
-
-      -- Integration with nvim-cmp
-      -- local cmp = require("cmp")
-      -- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      -- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
 }
