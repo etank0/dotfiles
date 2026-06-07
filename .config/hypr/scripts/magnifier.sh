b86c60410b66f5603a2d763027354a1f2edbfe1d
Swetank Upadhyay <swetank.690@gmail.com>
2026-06-07 10:46:27 +0530

    fix: zoom issue

--- .config/hypr/scripts/magnifier.sh
+++ .config/hypr/scripts/magnifier.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-ACTIVE_WIN="$(
-  hyprctl activewindow -j |
-  awk -F'"' '/"address":/ {print $4; exit}'
-)"
-
-hyprctl dispatch focuswindow "title:^$"
-
 step=1.5
+toggle_factor=2.25
 
 factor="$(
     hyprctl getoption cursor:zoom_factor |
@@ -18,22 +12,29 @@ factor="$(
 )"
 
 case "$1" in
-    wheel-down)
+    zoom-in)
         factor=$(awk "BEGIN { print $factor * $step }")
         ;;
-    wheel-up)
-        factor=$(awk "BEGIN { print $factor / $step }")
+
+    zoom-out)
+        factor=$(awk "BEGIN {
+            z = $factor / $step
+            print (z < 1 ? 1 : z)
+        }")
         ;;
+
+    toggle)
+        if awk "BEGIN { exit !($factor > 1.01) }"; then
+            factor=1
+        else
+            factor=$toggle_factor
+        fi
+        ;;
+
     *)
-        echo "usage: $0 {wheel-down|wheel-up}" >&2
-        [ -n "$ACTIVE_WIN" ] &&
-          hyprctl dispatch focuswindow "address:$ACTIVE_WIN"
+        echo "usage: $0 {toggle|zoom-in|zoom-out}" >&2
         exit 1
         ;;
 esac
 
 hyprctl keyword cursor:zoom_factor "$factor"
-
-if [ -n "$ACTIVE_WIN" ]; then
-    hyprctl dispatch focuswindow "address:$ACTIVE_WIN"
-fi
