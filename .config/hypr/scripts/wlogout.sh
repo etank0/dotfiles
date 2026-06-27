4094995eeea804670c0cf85770f8ef7cd3b04087
Swetank Upadhyay <swetank.690@gmail.com>
2026-06-27 22:57:26 +0530

    fix: vertical wlogout

--- .config/hypr/scripts/wlogout.sh
+++ .config/hypr/scripts/wlogout.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-A_1080=400
-B_1080=400
+A_1080=230
+B_1080=230
 
 # Check if wlogout is already running
 if pgrep -x "wlogout" > /dev/null; then
@@ -18,9 +18,19 @@ hypr_scale=$(
   hyprctl -j monitors | jq -r \
     '.[] | select(.focused==true) | .scale'
 )
+
+# Horizontal layout
+# wlogout \
+#   -C $HOME/.config/wlogout/material-darker-horizontal.css \
+#   -l $HOME/.config/wlogout/layout \
+#   --protocol layer-shell -b 5 \
+#   -T $(awk "BEGIN {printf \"%.0f\", $A_1080 * 1080 * $hypr_scale / $resolution}") \
+#   -B $(awk "BEGIN {printf \"%.0f\", $B_1080 * 1080 * $hypr_scale / $resolution}") &
+
+# Vertical layout
 wlogout \
-  -C $HOME/.config/wlogout/material-darker-edges.css \
+  -C $HOME/.config/wlogout/material-darker-vertical.css \
   -l $HOME/.config/wlogout/layout \
-  --protocol layer-shell -b 5 \
+  --protocol layer-shell -b 1 \
   -T $(awk "BEGIN {printf \"%.0f\", $A_1080 * 1080 * $hypr_scale / $resolution}") \
   -B $(awk "BEGIN {printf \"%.0f\", $B_1080 * 1080 * $hypr_scale / $resolution}") &
